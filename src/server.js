3134b2642c4e245f828e7497429287e25ccd4247
DhairyaG23 <dhairyag23@gmail.com>
2020-06-02 23:53:31 -0500

    updated for firebase with environment variables.

--- src/server.js
+++ src/server.js
@@ -1,2 +1,10 @@
 const fetch = require('node-fetch');
 
+const admin = require("firebase-admin");
+
+var serviceAccount = require(process.env.FB_TOKEN);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://frc-scouting-epicnd.firebaseio.com"
+});
