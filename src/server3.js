3e95be1421d96d9409b1e3f147c4ae2fbbae54a3
ccans <55026208+ccans@users.noreply.github.com>
2020-09-02 10:36:36 -0500

    pp

--- src/server3.js
+++ src/server3.js
@@ -2,15 +2,17 @@ const fetchPromises = require('node-fetch').promises;
 const fetch = require('node-fetch');
 
 const tbaKey = "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5";
-// const firebaseAdmin = require('firebase-admin')
-// const serviceAcc = require('../fbToken.json');
+const firebaseAdmin = require('firebase-admin')
+const serviceAcc = require('../fbToken.json');
 
-// var app = firebaseAdmin.initializeApp({
-//     credential: firebaseAdmin.credential.cert(serviceAcc),
-//     databaseURL: "https://frc-scouting-epicnd.firebaseio.com"
-// });
-// const database = app.database();
-// var db = firebaseAdmin.database();
+
+var app = firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAcc),
+    databaseURL: "https://frc-scouting-epicnd.firebaseio.com"
+  });
+const database = app.database();
+var db = firebaseAdmin.database();
+
 
 
 // This function is used to retrieve the events in a specified year, takes in the "year" parameter
@@ -48,13 +50,17 @@ async function getTeams(eventKey) {
 
 
 async function processTeamAtEvent(teamKey, eventKey) {
-    var resp = new Object;
+    var resp;
     await fetch(`https://www.thebluealliance.com/api/v3/team/${teamKey}/event/${eventKey}/matches?X-TBA-Auth-Key=${tbaKey}`)
         .then((response) => {
             return response.json();
         })
         .then(async (myJson) => {
-            await processData('2020', myJson, teamKey)
+            if(myJson == []) {
+                resp = null;
+            } else {
+                resp = await processData('2020', myJson, teamKey);
+            }
         })
 
     return resp;
@@ -71,16 +77,16 @@ async function processEvent(eventKey) {
     var eventTeams = await getTeams(eventKey);
     var max = eventTeams.length
     for (let i = 0; i < max; i++) {
-        console.log("rq #" + i + " sent");
         dummy(eventTeams[i], eventKey, max);
     }
 
     function check(maxValue) {
         if (counter == maxValue) {
-            //  console.log(processedEventObject);
-            //  console.log("done");
+            console.log("done" + eventKey);
+            pushDatatoFB("2020", eventKey, processedEventObject);
+
         } else {
-            //  console.log("recieved")
+
         }
     }
 
@@ -90,19 +96,41 @@ async function processEvent(eventKey) {
         check(maxValue);
     }
 
-    //    check(eventTeams.length)
+
 }
+
+function pushDatatoFB(year, event, data) {
+    var ref = db.ref("/"+ year);
+    ref.update({
+        [event] : data
+    })
+
+}
+
 
 async function process2020(data, teamKey) {
     var processed = new Object;
     //final score data
-    var teleOPAverage, autoAverage, innerAverage, outerAverage, totalAverage, winRate = 0;
+    var bottomAverage = 0;
+    var teleOPAverage = 0;
+    var autoAverage = 0;
+    var innerAverage = 0;
+    var outerAverage = 0;
+    var totalAverage = 0
+    var winRate = 0;
+
     var ties = 0;
     var wins = 0;
     //numbers used to calculate averages
-    var teleOPTotal, autoTotal, innerTotal, outerTotal, bottomTotal, totalTotal = 0;
+    var teleOPTotal = 0;
+    var autoTotal = 0;
+    var innerTotal = 0;
+    var outerTotal = 0;
+    var bottomTotal = 0;
+    var totalTotal = 0;
+
     var matchesPlayed = 0;
-    // if (teamKey === 'frc2264') console.log(data[0])
+
     for (let i = 0; i < data.length; i++) {
 
         if (data[i].winning_alliance) {
@@ -145,14 +173,37 @@ async function process2020(data, teamKey) {
         innerTotal += data.teleopCellsInner;
         bottomTotal += data.teleopCellsBottom;
         outerTotal += data.teleopCellsOuter;
-        if(teamKey === 'frc2264') console.log(totalTotal)
-        // console.log('handling data', [teamKey])
     }
         totalAverage = totalTotal/matchesPlayed;
-        winRate = (wins/(matchesPlayed - ties));
-        if(teamKey === 'frc2264') console.log(winRate);
+        winRate = (wins/matchesPlayed);
+        teleOPAverage = teleOPTotal/matchesPlayed;
+        autoAverage = autoTotal/matchesPlayed;
+        innerAverage = innerTotal/matchesPlayed;
+        bottomAverage = bottomTotal/matchesPlayed
+        outerAverage = outerTotal/matchesPlayed;
 
 
+
+        var returnObject = new Object;
+
+        returnObject["games"] = matchesPlayed;
+        returnObject["wlt"] = {
+            "wins": wins,
+            "losses": matchesPlayed - ties - wins,
+            "ties": ties
+        }
+        returnObject["averages"] = {
+            "avg": totalAverage.toFixed(2), 
+            "tOPAvg": teleOPAverage.toFixed(2), 
+            "autoAvg": autoAverage.toFixed(2), 
+            "innerAvg": innerAverage.toFixed(2), 
+            "bottomAvg": bottomAverage.toFixed(2), 
+            "outerAvg":outerAverage.toFixed(2),
+            "wltRate": winRate.toFixed(2)
+        }
+
+        return returnObject;
+        // console.log(returnObject);
 }
 
 async function processData(year, data, teamKey) {
@@ -176,15 +227,21 @@ async function processAllEventsInYear(year) {
 
     console.log(eventKeys);
 
+    for(var i = 0; i < eventKeys.length; i++) {
+      await processEvent(eventKeys[i]);
+    }
 
-    console.log(teamKeys);
 
 }
+
+
 
 
 async function deleteAllDataAtEndpoint(endpoint) {
 
 }
 
-// processAllEvents();
-processEvent('2020mndu')
+processAllEventsInYear(2020);
+
+// processEvent('2020mndu2')
+// processEvent('2020mnmi')
